025a73c70ae8194e1f9b8e532dbf20a2370f09c4
Brian Lowell <brian.lowell@gmail.com>
2017-06-10 11:05:55 -0500

    added fiduciaries to report, fixed buttons

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@ class ReportsController < ApplicationController
   def report
     @matter = Matter.find(params[:id])
     @assets = @matter.assets
-    
+    @fiduciaries = @matter.fiduciaries
 
     render("matters/report.html.erb")
   end
